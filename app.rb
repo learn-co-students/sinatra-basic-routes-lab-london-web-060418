b2b9a8d5b1fd1593d7ba0d2c386b615945f5087f
Allegra <aukseallegra@gmail.com>
2018-06-24 20:12:38 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do          # http://localhost:9393/
+    "Hello, World!"
+  end
+
+  get '/name' do       # http://localhost:9393/name
+    "My name is Allegra"
+  end
+
+  get '/hometown' do    # http://localhost:9393/hometown
+    "My hometown is Planet Earth."
+  end
+
+  get '/favorite-song' do    # http://localhost:9393/favorite-song
+    "My favorite song is silence."
+  end
+
 end
